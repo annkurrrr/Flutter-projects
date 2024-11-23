973497fb9c1bf05972ac5613e75e67efa90f42b1
annkurrrr <ankur.kunde@gmail.com>
2024-11-23 09:59:42 +0530

    logout and stuff added

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tasks/firebase_options.dart';
 import 'package:tasks/screens/login_view.dart';
 import 'package:tasks/screens/signup.dart';
 import 'package:tasks/screens/verify_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -70,16 +71,47 @@ class _TasksState extends State<Tasks> {
           titleTextStyle: const TextStyle(
               fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
           actions: [
-            PopupMenuButton<MenuAction>(
-                onSelected: (value) {},
-                itemBuilder: (context) {
-                  return const [
-                    PopupMenuItem(
-                        value: MenuAction.logout, child: Text('Logout')),
-                  ];
-                }),
+            PopupMenuButton<MenuAction>(onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  devtools.log(shouldLogout.toString());
+                  if (shouldLogout) {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login/', (_) => false);
+                  }
+              }
+            }, itemBuilder: (context) {
+              return const [
+                PopupMenuItem(value: MenuAction.logout, child: Text('Logout')),
+              ];
+            }),
           ],
         ),
         body: const Text('hello world'));
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Sign out"),
+          content: const Text("Are you sure you wanna sign out"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text("Cancel")),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text("Log out")),
+          ],
+        );
+      }).then((value) => value ?? false);
 }
