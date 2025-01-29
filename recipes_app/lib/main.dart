121cf21bc49658df19d6e2be428e5fdfe5f04652
annkurrrr <ankur.kunde@gmail.com>
2025-01-29 22:49:48 +0530

    recipes app part 7

--- recipes_app/lib/main.dart
+++ recipes_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:recipes_app/screens/add_recipe.dart';
 import 'package:recipes_app/screens/home.dart';
 
 void main() {
@@ -9,16 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Recipes app',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: Home(),
     );
   }
