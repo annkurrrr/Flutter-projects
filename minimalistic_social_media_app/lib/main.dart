5494fbc11f12cca926373c2656d22776cfc7dedd
annkurrrr <ankur.kunde@gmail.com>
2025-02-14 18:06:21 +0530

    signin and register suing firebase donw

--- minimalistic_social_media_app/lib/main.dart
+++ minimalistic_social_media_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:minimalistic_social_media_app/auth/login_or_register.dart';
+import 'package:minimalistic_social_media_app/auth/auth.dart';
 import 'package:minimalistic_social_media_app/firebase_options.dart';
 import 'package:minimalistic_social_media_app/themes/dark_mode.dart';
 import 'package:minimalistic_social_media_app/themes/light_mode.dart';
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
     );
