e80185ac591a70357be134f504a0e35ce206e2c8
annkurrrr <ankur.kunde@gmail.com>
2025-02-15 14:02:14 +0530

    signin and register done

--- minimalistic_social_media_app/lib/pages/home_page.dart
+++ minimalistic_social_media_app/lib/pages/home_page.dart
@@ -1,13 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Home"),
+            IconButton(
+              onPressed: logout,
+              icon: Icon(Icons.logout),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.grey[800],
       ),
     );
   }
